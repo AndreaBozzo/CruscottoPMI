4cb28b37a9f39ea630ca954e516d693699bf5a3f
Andrea Bozzo <andreabozzo92@gmail.com>
2025-05-08 17:44:22 +0200

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotlib.express as px
 from io import BytesIO
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
