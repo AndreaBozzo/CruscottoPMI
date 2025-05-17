8bdc50f294eb2237805bd02dbb727c2fc9cdae9a
poinT92 <andreabozzo92@gmail.com>
2025-05-17 16:58:24 +0200

    Push completo progetto v0.5 con struttura finale e configurazione Cloud

--- run_app.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-# ✅ Imposta PYTHONPATH per permettere gli import da 'src'
-os.environ["PYTHONPATH"] = "src"
-
-# ✅ Avvia Streamlit dalla pagina principale
-subprocess.run(["streamlit", "run", "00_home.py"])
