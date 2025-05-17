bdf191e5ad7cce12831c0ff128f628d662ac2906
poinT92 <andreabozzo92@gmail.com>
2025-05-17 17:10:09 +0200

    ✅ Fix finale struttura: streamlit_app + pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,2 @@
-
-import sys
-import os
-
-# Aggiunge src/ al path per import cruscotto_pmi.*
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-
 import streamlit as st
-
-# Reindirizza alla pagina principale multipagina
 st.switch_page("pages/00_home.py")
